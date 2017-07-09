d7ee620b73a444a8d338c7f93f27e13597a6e75b
Colin Fausnaught <cfausn@gmail.com>
2017-07-09 16:36:26 -0400

    added whitepaper

--- js/custom.js
+++ js/custom.js
@@ -7,12 +7,12 @@ $(document).ready(function() {
       if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
         var target = $(this.hash);
         target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-        
+
         if (target.length) {
           $('html, body').animate({
             scrollTop: target.offset().top
           }, 1000, 'easeInOutExpo');
-          
+
           if ( $(this).parents('.nav-menu').length ) {
             $('.nav-menu .menu-active').removeClass('menu-active');
             $(this).closest('li').addClass('menu-active');
@@ -34,7 +34,7 @@ $(document).ready(function() {
     animation: {opacity:'show'},
     speed: 400
   });
-  
+
   // Mobile Navigation
   if( $('#nav-menu-container').length ) {
       var $mobile_nav = $('#nav-menu-container').clone().prop({ id: 'mobile-nav'});
@@ -43,19 +43,19 @@ $(document).ready(function() {
       $('body').prepend( '<button type="button" id="mobile-nav-toggle"><i class="fa fa-bars"></i></button>' );
       $('body').append( '<div id="mobile-body-overly"></div>' );
       $('#mobile-nav').find('.menu-has-children').prepend('<i class="fa fa-chevron-down"></i>');
-      
+
       $(document).on('click', '.menu-has-children i', function(e){
           $(this).next().toggleClass('menu-item-active');
           $(this).nextAll('ul').eq(0).slideToggle();
           $(this).toggleClass("fa-chevron-up fa-chevron-down");
       });
-      
+
       $(document).on('click', '#mobile-nav-toggle', function(e){
           $('body').toggleClass('mobile-nav-active');
           $('#mobile-nav-toggle i').toggleClass('fa-times fa-bars');
           $('#mobile-body-overly').toggle();
       });
-      
+
       $(document).click(function (e) {
           var container = $("#mobile-nav, #mobile-nav-toggle");
           if (!container.is(e.target) && container.has(e.target).length === 0) {
@@ -69,10 +69,10 @@ $(document).ready(function() {
   } else if ( $("#mobile-nav, #mobile-nav-toggle").length ) {
       $("#mobile-nav, #mobile-nav-toggle").hide();
   }
-  
+
   // Stick the header at top on scroll
   $("#header").sticky({topSpacing:0, zIndex: '50'});
-  
+
   // Counting numbers
 
   $('[data-toggle="counter-up"]').counterUp({
@@ -83,23 +83,23 @@ $(document).ready(function() {
   // Tooltip & popovers
   $('[data-toggle="tooltip"]').tooltip();
   $('[data-toggle="popover"]').popover();
-  
+
   // Background image via data tag
   $('[data-block-bg-img]').each(function() {
     // @todo - invoke backstretch plugin if multiple images
     var $this = $(this),
       bgImg = $this.data('block-bg-img');
-      
+
       $this.css('backgroundImage','url('+ bgImg + ')').addClass('block-bg-img');
   });
-  
+
   // jQuery counterUp
   if(jQuery().counterUp) {
     $('[data-counter-up]').counterUp({
       delay: 20,
     });
   }
-  
+
   //Scroll Top link
   $(window).scroll(function(){
     if ($(this).scrollTop() > 100) {
@@ -108,7 +108,7 @@ $(document).ready(function() {
       $('.scrolltop').fadeOut();
     }
   });
- 
+
   $('.scrolltop, #logo a').click(function(){
     $("html, body").animate({
       scrollTop: 0
@@ -116,4 +116,96 @@ $(document).ready(function() {
     return false;
   });
 
+});
+
+
+console.log("nothing to see here, move along");
+
+function decodeStats(response, price) {
+    if (response == null) return null;
+
+    var result = response.result;
+    if (result == null || result.length == null || result.length < 193) return null;
+
+    var weiPerEther = new BigNumber("1000000000000000000", 10);
+
+    var totalContributionExact = new BigNumber(result.substr(2, 64), 16).div(weiPerEther);
+    var totalContributionUSDExact = totalContributionExact.times(new BigNumber(price));
+
+    return {
+        totalContribution: totalContributionExact.round(3, BigNumber.ROUND_DOWN),
+        totalContributionUSD: totalContributionUSDExact.round(0, BigNumber.ROUND_DOWN),
+        totalContributionTVs: totalContributionUSDExact.div(new BigNumber("1200")).round(0, BigNumber.ROUND_DOWN),
+        totalSupply: new BigNumber(result.substr(66, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        totalBonusTokensIssued: new BigNumber(result.substr(130, 64), 16).div(weiPerEther).round(3, BigNumber.ROUND_DOWN),
+        purchasingAllowed: new BigNumber(result.substr(194, 64), 16).isZero() == false
+    };
+}
+
+function getStats(price) {
+    var url = "https://api.etherscan.io/api?module=proxy&action=eth_call&to=0x962F57e8131A39c592485746373D1d80768e548f&data=0xc59d48470000000000000000000000000000000000000000000000000000000000000000&tag=latest";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) { return decodeStats(data, price); });
+}
+
+function getPrice() {
+    var url = "https://api.etherscan.io/api?module=stats&action=ethprice";
+    return $.ajax(url, {
+        cache: false,
+        dataType: "json"
+    }).then(function (data) {
+        if (data == null) return null;
+        if (data.result == null) return null;
+        if (data.result.ethusd == null) return null;
+
+        return parseFloat(data.result.ethusd);
+    });
+}
+
+function updatePage(stats) {
+    if (stats == null) return;
+    if (!stats.purchasingAllowed/* && Date.now < Date.UTC(2017, 6, 4, 14)*/) {
+        $("#stats").hide();
+        return;
+    }
+
+    $("#total-ether").text(stats.totalContribution.toFixed(3));
+    if (stats.totalContribution.toNumber() <= 0) {
+        $("#total-ether-message").text("Looks like everyone read the warnings so far.");
+    } else {
+        $("#total-ether-message").text("I had a feeling someone would waste their money.");
+    }
+
+    $("#total-usd").text("$" + stats.totalContributionUSD.toFixed(0));
+    if (stats.totalContributionUSD.toNumber() <= 0) {
+        $("#total-usd-message").text("No Ether yet, so no cash either.");
+    } else if (stats.totalContributionTVs.toNumber() < 1) {
+        $("#total-usd-message").text("Not enough to buy a television yet.");
+    }else if (stats.totalContributionTVs.toNumber() < 2) {
+        $("#total-usd-message").text("Enough to buy a television.");
+    } else {
+        $("#total-usd-message").text("Enough to buy " + stats.totalContributionTVs.toFixed(0) + " televisions!");
+    }
+
+    $("#total-tokens").text(stats.totalSupply.toFixed(3));
+    if (stats.totalSupply <= 0) {
+        $("#total-tokens-message").text("No useless tokens issued yet either.");
+    } else if (stats.totalBonusTokensIssued.toNumber() <= 0) {
+        $("#total-tokens-message").text("Look at all those useless tokens!");
+    } else {
+        $("#total-tokens-message").text("Including " + stats.totalBonusTokensIssued.toFixed(3) + " bonus tokens!");
+    }
+
+    $("#stats").show();
+}
+
+function refresh() { getPrice().then(getStats).then(updatePage); }
+
+$(function() {
+    try {
+        refresh();
+        setInterval(refresh, 1000 * 60 * 5);
+    } catch (err) { }
 });
